66d88ef3cf0adce6ec6f51d89fc0dcdfa45735f9
Giovanni Filippini <39963191+giovannifil-64@users.noreply.github.com>
2023-12-02 14:49:54 +0100

    - Refactored JSON decoding in cli.py - Updated the .gitignore to avoid pushing pycache - Upsates to the CLI

--- ChatSQL/code/cli.py
+++ ChatSQL/code/cli.py
@@ -5,18 +5,18 @@ from jsonDecoder import json_decoder
 def main():
     print("Welcome to the ChatSQL CLI")
     while True:
-        inputFile = input("Enter a filename: ")
+        inputFile = input("Enter a file to start: ")
         if not os.path.exists(inputFile):
-            print("File does not exist.")
+            print("The file does not exist.")
         else:
             try:
                 with open(inputFile, "r") as f:
                     contents = f.read()
-                    json_data = json.loads(contents)  # Parse the JSON data
+                    json_data = json_decoder(contents)
             except Exception as e:
                 print("Error reading or decoding JSON file:", e)
             else:
-                code = json_decoder(json_data)
+                code = json_data
                 print("Generated code:", code)
                 break
 
--- ChatSQL/code/jsonDecoder.py
+++ ChatSQL/code/jsonDecoder.py
@@ -9,12 +9,13 @@ def json_decoder(json_data):
             description = column.get("description", "")
             reference_info = f" (foreign key referencing {column['references']})" if "references" in column else ""
             code.append(f"emb.upsert((len(code), {{'text': '{description}{reference_info}', 'campo': '{column_name}', 'tabella': '{table_name}'}}))")
-
     return code
 
+'''
 if __name__ == "__main__":
     with open("ChatSQL/res/data.json") as f:
         json_data = json.load(f)
 
 code = json_decoder(json_data)
 print(code)
+'''
