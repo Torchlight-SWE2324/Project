086d8c2f43361ff08dad2dd6c6b12a984db50816
Giovanni Filippini <39963191+giovannifil-64@users.noreply.github.com>
2023-12-11 16:56:15 +0100

    - Add import statements and call the "emb" function to embed data

--- ChatSQL/code/cliUser.py
+++ ChatSQL/code/cliUser.py
@@ -1,17 +1,19 @@
 import os
 import sys
 
-from fileOperations import getFiles
-
 dirPath = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.abspath(os.path.join(dirPath, "..")))
+
+from fileOperations import getFiles
+from embedder import emb
 
 def user():
     os.system('clear')
     print("Welcome! Please choose a file to chat in:" + getFiles())
     #retrive the data
 
-    
+    #embed the data
+    emb(retriveFile())
 
 #get a file from the database
 def retriveFile(filename):
