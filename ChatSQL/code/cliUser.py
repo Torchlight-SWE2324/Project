51d561d439bb2a905072f3285438d83dc10f822a
Giovanni Filippini <39963191+giovannifil-64@users.noreply.github.com>
2023-12-11 17:19:39 +0000

    - Updated user input to ask for a file until a valid one is provided. - Implemented file existence checking and added the .json extension if necessary. - Improved user experience by providing informative error messages.

--- ChatSQL/code/cliUser.py
+++ ChatSQL/code/cliUser.py
@@ -11,11 +11,30 @@ def user():
     os.system('clear')
     print("Welcome! Please choose a file to chat in:" + getFiles())
     
-    filename = input("Select a file:")
     JSON_path = os.path.abspath(os.path.join(dirPath, os.pardir, "JSON"))
-    json_file_path = os.path.join(JSON_path, filename)
+    
+    while True:
+        # Ask for a file
+        filename = input("Select a file: ")
+
+        # Construct the initial file path
+        json_file_path = os.path.join(JSON_path, filename)
+
+        # Check if the file exists
+        if not os.path.exists(json_file_path):
+            # If the file doesn't exist, try adding the .json extension
+            json_file_path = os.path.join(JSON_path, f"{filename}.json")
+
+            # Check again if the file exists
+            if not os.path.exists(json_file_path):
+                print(f"Error: The file '{filename}' or '{filename}.json' does not exist. Please check the path.")
+                continue  # Ask for the file again
+
+        # If a valid file is found, break out of the loop
+        break
 
     emb(json_file_path)
+
 
 if __name__ == "__main__":
     user()
