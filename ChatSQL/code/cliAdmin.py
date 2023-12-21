2dd7a2ef7c4fede2252b830dbffc1b7c1ac50d0e
Giovanni Filippini <39963191+giovannifil-64@users.noreply.github.com>
2023-12-21 09:00:42 +0100

    Small refactor in the admin section

--- ChatSQL/code/cliAdmin.py
+++ ChatSQL/code/cliAdmin.py
@@ -1,35 +1,36 @@
-import re
-
 from fileOperations import getFiles, addFile, deleteFile
 
 def admin():
     print("\n\033[1mWelcome to the admin section\033[0m")
     while True:
         print("\nWhat do you want to do?")
-        print("1. Add file")
-        print("2. Delete file")
-        print("3. Get files")
+        print("1. Add a file")
+        print("2. Delete a file")
+        print("3. Get all the file sin the database")
         print("4. Leave the admin section")
 
         choice = input("Your choice: ").lower()
 
-        if re.match(r"^1$|^add$", choice):
+        if choice == "1" or choice == "add":
             addFile()
-        elif re.match(r"^2$|^delete$", choice):
+        elif choice == "2" or choice == "delete":
             deleteFile()
-        elif re.match(r"^3$|^get$", choice):
+        elif choice == "3" or choice == "get":
             print("Files in the database:" + getFiles())
-        elif re.match(r"^4$|^exit$", choice):
-            confirmation = input("Are you sure you want to exit the admin section? (yes/no): ").lower()
-            if confirmation.lower() in ["yes", "y"]:
-                print("Leaving admin section. You will be redirected to the main menu.")
-                break
-            elif confirmation.lower() in ["no", "n"]:
-                print("Returning to the admin section menu.")
-            else:
-                print("Invalid choice. Returning to the admin section menu.")
+        elif choice == "4" or choice == "leave":
+            handle_exit()
         else:
             print("Invalid choice")
+
+def handle_exit():
+    confirmation = input("Are you sure you want to leave the admin section? (yes/no): ").lower()
+    if confirmation == "yes" or confirmation == "y":
+        print("Leaving admin section. You will be redirected to the main menu.")
+    elif confirmation == "no" or confirmation == "n":
+        print("Returning to the admin section menu.")
+    else:
+        print("Invalid choice. Returning to the admin section menu.")
+
 
 if __name__ == "__main__":
     print("Files in the database:" + getFiles())
