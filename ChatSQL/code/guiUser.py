0e2065bd011dd66dfffa756107fac4fa096f0b6d
Fabio Meneghini <fabio.meneghini.1@studenti.unipd.it>
2023-12-27 00:10:30 +0100

    Refactored the GUI, now the sidebar is being used

---
+++ ChatSQL/code/guiUser.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import time
+from fileOperations import getFiles
+
+def answer(assistant_response):
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        # Simulate stream of response with milliseconds delay
+        for character in assistant_response:
+            full_response += character
+            time.sleep(0.015)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+        st.session_state.chat.append({"role": "assistant", "content": full_response})
+
+def admin():
+    st.session_state.chat.append({"role": "assistant", "content": "Access the admin section"})
+
+def exit():
+    st.session_state.chat.append({"role": "assistant", "content": "Exit the program"})
+
+def guiUser():
+    st.title("ChatSQL")
+    st.subheader("Type your natural language query in the chat box below and press enter to get the corresponding SQL query. To access the admin section or exit the program, use the buttons on the sidebar.")
+
+    with st.sidebar:
+        button_admin = st.button("Admin section", help="Access the admin section to upload or delete a data dictionary file",
+                                 on_click=admin, type="primary", use_container_width=False, disabled=False, key=None)
+        option = st.selectbox('Data dictionary file:',
+                             ('Dizionario1', 'Dizionario2', 'Dizionario3'))
+        st.write("***")
+        button_exit = st.button("Exit", help="Exit the program :(", on_click=exit, type="secondary", use_container_width=False, disabled=False, key=None)
+
+    # Initialize chat history
+    if "chat" not in st.session_state:
+        st.session_state.chat = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.chat:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # React to user input
+    if prompt := st.chat_input("Insert natural language query here"):
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # Add user message to chat history
+        st.session_state.chat.append({"role": "user", "content": prompt})
+
+        response = f"Echo: {prompt}"
+
+        # Display assistant response in chat message container
+        answer("Messaggio di prova")
+
+if __name__ == "__main__":
+    guiUser()
