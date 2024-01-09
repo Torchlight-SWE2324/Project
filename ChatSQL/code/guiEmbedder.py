890ef34f5623f878f822bdf63df4bbbecdeba568
Giovanni Filippini <39963191+giovannifil-64@users.noreply.github.com>
2024-01-09 12:23:23 +0000

    - Readded password file - Reintroduced score for admin only

--- ChatSQL/code/guiEmbedder.py
+++ ChatSQL/code/guiEmbedder.py
@@ -5,7 +5,6 @@ import streamlit as st
 from guiUtils import generateUpsertCommands, upsert
 from txtai import Embeddings
 
-
 def loadIndex(dictionary_file_name):
     if st.session_state.emb == None:
         st.session_state.emb = Embeddings({"path": "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2", "content": True})
@@ -31,10 +30,9 @@ def generatePromptAdmin(emb, user_query):
     if emb == None:
         return "Error: there is no model connected"
 
-    embedder_search_result = emb.search(f"select score, text, table_name, table_description, field_name, field_type, field_references, "
-                         f" from txtai where similar('{user_query}') limit 30") #da definire il limite
+    embedder_search_result = emb.search(f"select score, text, table_name, table_description, field_name, field_type, field_references from txtai where similar('{user_query}') limit 30") #da definire il limite
 
-    prompt=""
+    prompt = ""
     for result in embedder_search_result:
         prompt = prompt+"\n"+str(result)+"\n"
 
@@ -44,13 +42,18 @@ def generatePromptUser(emb, user_query):
     if emb == None:
         return "Error: there is no model connected"
 
-    embedder_search_result = emb.search(f"select score, text, table_name, table_description, field_name, field_type, field_references, "
-                         f" from txtai where similar('{user_query}') limit 30") #!!! DA SOSTITUIRE "LIMIT 30"
+    embedder_search_result = emb.search(f"select score, text, table_name, table_description, field_name, field_type, field_references, from txtai where similar('{user_query}') limit 30") #!!! DA SOSTITUIRE "LIMIT 30"
 
     selected_fields_list = {}
     relationship_list = []
-
+    
     for result in embedder_search_result:
+        ret = ""
+
+        if st.session_state.logged_in == True:
+            ret = "SCORE FOR DEBUGGING ONLY\n"
+            ret += f"Score: {result['score']}\n"
+
         table_name = result['table_name']
         field_name = result['field_name']
 
@@ -64,18 +67,17 @@ def generatePromptUser(emb, user_query):
             relationship_list.append(f"'{table_name}.{field_name}' references '{references['table_name']}."
                             f"{references['field_name']}';\n")
 
-    prompt = f"\n\nThe database contains the following tables:\n\n"
+    prompt = f"{ret}\nThe database contains the following tables:\n"
 
     for table, fields in selected_fields_list.items():
         field_str = ', '.join([f"'{field}'" for field in fields])
-        prompt += f"'{table}' with fields {field_str};\n\n"
-
+        prompt += f"'{table}' with fields {field_str};\n"
 
     if relationship_list:
         prompt += "\nand the database contains the following relationships:\n"
 
         for relation in relationship_list:
-            prompt += "\n"+relation
+            prompt += relation
         prompt += f"\nGenerate the SQL query equivalent to: {user_query}"
 
     return prompt
