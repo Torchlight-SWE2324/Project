1b9b97894b9678334c46e74f0da6c3bea0778652
Dmitry358 <Dmitry358@github.com>
2024-01-10 16:15:42 +0100

    Migliorato contenutot del prompt

--- ChatSQL/code/guiEmbedder.py
+++ ChatSQL/code/guiEmbedder.py
@@ -42,44 +42,67 @@ def generatePromptUser(emb, user_query):
     if emb == None:
         return "Error: there is no model connected"
 
-    embedder_search_result = emb.search(f"select score, text, table_name, table_description, field_name, field_type, field_references, from txtai where similar('{user_query}') limit 30") #!!! DA SOSTITUIRE "LIMIT 30"
+    #embedder_search_result = emb.search(f"select score, text, table_name, table_description, field_name, field_type, field_references, from txtai where similar('{user_query}') limit 30") #!!! DA SOSTITUIRE "LIMIT 30"
 
-    selected_fields_list = {}
-    relationship_list = []
+    prompt =''
 
-    ret = ""
-    #?? DA METTERE " ret = "" " qua?
-    for result in embedder_search_result:
+    if st.session_state.logged_in == True:
+        '''
+        for result in embedder_search_result:
+            prompt = prompt+"\n"+str(result)+"\n"
+        return prompt
+        '''
+        embedder_search_result = emb.search(f"select score, text, table_name, table_description, field_name, field_type, field_references, from txtai where similar('{user_query}') limit 30") #!!! DA SOSTITUIRE "LIMIT 30"
+
+        for result in embedder_search_result:
+            prompt += f"'{result['table_name']}.{result['field_name']}' have score: {result['score']};\n"
+
+        return prompt
+
+    else:
+        embedder_search_result = emb.search(f"select score, text, table_name, table_description, field_name, field_type, "
+                                            f"field_references, from txtai where similar('{user_query}') and score >= 0.4")
+
+        selected_fields_list = {}
+        relationship_list = []
+
+        ret = ""
+        for result in embedder_search_result:
+            table_name = result['table_name']
+            field_name = result['field_name']
+
+            if table_name in selected_fields_list:
+                selected_fields_list[table_name].append(field_name)
+            else:
+                selected_fields_list[table_name] = [field_name]
+
+            if table_name == result['table_name'] and field_name == result['field_name'] and result['field_references']:
+                references = json.loads(result['field_references'])
+                relationship_list.append(f"'{table_name}.{field_name}' references '{references['table_name']}."
+                                f"{references['field_name']}';\n")
 
 
-        if st.session_state.logged_in == True:
-            ret = "SCORE FOR DEBUGGING ONLY\n"
-            ret += f"Score: {result['score']}\n"
+        prompt = ''
+        prompt_fields_list = ''
 
-        table_name = result['table_name']
-        field_name = result['field_name']
+        for table, fields in selected_fields_list.items():
+            field_str = ', '.join([f"'{field}'" for field in fields])
+            prompt_fields_list += f"'{table}' with fields {field_str};\n"
 
-        if table_name in selected_fields_list:
-            selected_fields_list[table_name].append(field_name)
+        if prompt_fields_list:
+            prompt += f"{ret}\nThe database contains the following tables:\n"
+            prompt += prompt_fields_list
+
+            if relationship_list:
+                prompt_relationship_list = "\nand the database contains the following relationships:\n"
+                for relation in relationship_list:
+                    prompt_relationship_list += relation
+
+                prompt += prompt_relationship_list
+
+            prompt += f"\nGenerate the SQL query equivalent to: {user_query}"
+
         else:
-            selected_fields_list[table_name] = [field_name]
+            prompt += "No relevant information was found in relation to the request"
 
-        if table_name == result['table_name'] and field_name == result['field_name'] and result['field_references']:
-            references = json.loads(result['field_references'])
-            relationship_list.append(f"'{table_name}.{field_name}' references '{references['table_name']}."
-                            f"{references['field_name']}';\n")
-
-    prompt = f"{ret}\nThe database contains the following tables:\n"
-
-    for table, fields in selected_fields_list.items():
-        field_str = ', '.join([f"'{field}'" for field in fields])
-        prompt += f"'{table}' with fields {field_str};\n"
-
-    if relationship_list:
-        prompt += "\nand the database contains the following relationships:\n"
-
-        for relation in relationship_list:
-            prompt += relation
-        prompt += f"\nGenerate the SQL query equivalent to: {user_query}"
-
-    return prompt
+        return prompt
