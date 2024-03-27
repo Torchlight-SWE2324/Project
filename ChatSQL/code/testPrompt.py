356f1f0b4d14dcac5cf92493cb58db933972d847
Giovanni Filippini <39963191+giovannifil-64@users.noreply.github.com>
2024-03-27 17:57:23 +0100

    Co-authored-by: Tao Ren Federico Ye <FedeYe@users.noreply.github.com> Co-authored-by: MarcoCappellari <MarcoCappellari@users.noreply.github.com>

--- ChatSQL/code/testPrompt.py
+++ ChatSQL/code/testPrompt.py
@@ -32,14 +32,18 @@ def deleteIndex(dirPath, filename_to_delete_without_extension):
 def generatePrompt(emb, user_query, dictionary_name):
     #set logged_in to true
     demo_login = False
+
+    dictionary_path = os.path.join(getDictionariesFolderPath(), dictionary_name)
+
+    with open(dictionary_path, 'r') as dictionary_file:
+        data = json.load(dictionary_file)
+
+
+
+
     # Creazione prompt per Admin
     if demo_login:
         embedder_search_result = emb.search(f"SELECT score, text, table_name, field_name, field_description FROM txtai WHERE similar('{user_query}') AND score > 0.01 GROUP BY table_name, field_name", limit=50)
-
-        dictionary_path = os.path.join(getDictionariesFolderPath(), dictionary_name)
-
-        with open(dictionary_path, 'r') as dictionary_file:
-            data = json.load(dictionary_file)
 
         tables_with_fields = {}
 
@@ -65,58 +69,52 @@ def generatePrompt(emb, user_query, dictionary_name):
 
     # Creazione prompt per User
     else:
-        embedder_search_result = emb.search(f"SELECT score, text, table_name, field_name, field_description, field_references FROM txtai WHERE similar('{user_query}') AND score > 0.25 GROUP BY table_name, field_name", limit=50)
-        
+        embedder_search_result = emb.search(f"select score, text, table_name, table_description, field_name, field_type, field_references, from txtai where similar('{user_query}') and score > 0.25 group by table_name")
+
         prompt = ""
         
-        tables_with_fields = {}
-        referencies_list = {}
+        tables_with_fields_list = []
+        referencies_list = []
 
         for result in embedder_search_result:
-            table_name = result['table_name']
-            field_name = result['field_name']
-            field_description = result['field_description']
-            if # controlla se ci sono references 
-                ref_table_name = result['field_references']['table_name']
-                ref_field_name = result['field_references']['field_name']
+            for table in data["tables"]:
+                if table["name"] == result['table_name']:
+                    table_with_fields = {"table_name": table["name"], "fields_list": []}
 
-            if table_name not in tables_with_fields:
-                tables_with_fields[table_name] = []
-            tables_with_fields[table_name].append((field_name, field_description))
+                    for column in table["columns"]:
+                        field_with_description = {"field_name" : column["name"], "field_description" : column["description"]}
+                        table_with_fields["fields_list"].append(field_with_description)
 
-            if table_name not in referencies_list:
-                referencies_list[table_name] = []
-            referencies_list[table_name].append((table_name, field_name, ref_table_name, ref_field_name))
+                        if column["references"] != None:
+                            print(column["references"])
+                            referencies_list.append(f"'{table['name']}.{column['name']}' references {column['references']['table_name']}.{column['references']['field_name']}';\n")
 
+                    tables_with_fields_list.append(table_with_fields)
 
-        
-        if len(tables_with_fields) > 0:
+        if len(tables_with_fields_list) > 0:
             prompt = "The data base contain the following tables:\n\n"
 
-            for table_name, fields in tables_with_fields.items():
-                prompt += f"TABLE '{table_name}':\n"
-                for field, description, score in fields:
-                    prompt += f"- FIELD: {field} {' '*(max(len(field), 12) - len(field))} | DESCRIPTION: {description}\n"
+            for table in tables_with_fields_list:
+                prompt += f"table '{table['table_name']}' with fields:\n"
+
+                for field in table['fields_list']:
+                    prompt += f"'{field['field_name']}' that contain {field['field_description']};\n"
+                prompt += "\n"
 
             if len(referencies_list) > 0:
                 prompt += "and the database contains the following relationships:\n"
-                for table_name, fields in tables_with_fields.items():
-                    prompt += f"TABLE '{table_name}':\n"
-                    for tname, fname, rtname, rfname in fields:
-                        prompt += f"{tname}.{fname} references {rtname}.{rfname}\n"
+                for reference in referencies_list:
+                    prompt += reference
+
 
             prompt += f"\nGenerate the SQL query equivalent to: {user_query}"
-        
-        else:
-            prompt = "No relevant information was found regarding your request. \nPlease try again with a different query. \nPlease note that this application is designed to handle requests that can be translated into a SQL query."
-        
-        return prompt
+            return prompt
 
 if __name__ == "__main__":
 
     emb = Embeddings({"path": "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2", "content": True})
 
-    with open("C:/Users/taore/Documents/Codice/SWE/ChatSQL/ChatSQL/JSON/auction.json", 'r') as file:
+    with open("C:/Users/Marco/Desktop/swe/progetto/ChatSQL/ChatSQL/database/auction.json", 'r') as file:
         data = json.load(file)
 
     commands = []
@@ -146,26 +144,4 @@ if __name__ == "__main__":
                 "field_description": command["field_description"],
                 "text": command["field_description"]} for command in commands])    # va cambiato il campo text dentro emb.index
     
-    print(generatePrompt(emb, user_query="item", dictionary_name="C:/Users/taore/Documents/Codice/SWE/ChatSQL/ChatSQL/JSON/auction.json"))
-
-
-
-
-
-
-
-    # utente può cambiare il dizionario corrente
-    # bottone selezione nuovo dizionario corrente
-    # controller riceve il segnale di cambio dizionario corrente
-    # nel model va cambiato l'index dell'embedding 
-    # emb.load(va a recuperare l'index dentro la cartella index)
-
-
-    # Caricamento
-    
-    # DizionariSalvatiModel
-    # getDizionari() aprire la cartella database e leggere da lì la lista //evito così il sigleton
-    # crei una lista che ritorni al controller -> a sua volta li mostra sulla view
-    
-    # isLogged session state
-    # percorso corrente session state
+    print(generatePrompt(emb, user_query="bid", dictionary_name="C:/Users/Marco/Desktop/swe/progetto/ChatSQL/ChatSQL/database/auction.json"))
