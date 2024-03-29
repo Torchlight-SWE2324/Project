c412b84c404040d7713e911a32a0c615d384bb55
MarcoCappellari <marco.cap.blu@gmail.com>
2024-03-29 11:19:19 +0100

    sistemata chat tecnico

--- ChatSQL/Start/ResponseGenerator.py
+++ ChatSQL/Start/ResponseGenerator.py
@@ -62,52 +62,26 @@ class ResponseTechnician:
         embe = emb.getEmb()
         emb.caricareIndex(dictionary_name)
         
-        with open(os.path.join(os.path.dirname(__file__), "database", dictionary_name), 'r') as file:
-            print("FILE:", file)
+        embedder_search_result = embe.search(f"SELECT score, text, table_name, field_name, field_description FROM txtai WHERE similar('{user_query}') AND score > 0.01 GROUP BY table_name, field_name", limit=50)
 
-            data = json.load(file)
-            embedder_search_result = embe.search(f"select score, text, table_name, table_description, field_name, field_type, field_references, from txtai where similar('{user_query}') and score > 0.01 group by table_name")
-            
-            tables_with_fields_list = []
-            referencies_list = []
+        tables_with_fields = {}
 
-            prompt = '\n'
-            for result in embedder_search_result:
-                prompt += f"Table '{result['table_name']}' have score: {result['score']};\n"
+        for result in embedder_search_result:
+            table_name = result['table_name']
+            field_name = result['field_name']
+            field_description = result['field_description']
+            score = result['score']
 
-            for result in embedder_search_result:
-                for table in data["tables"]:
-                    if table["name"] == result['table_name']:
-                        table_with_fields = {"table_name": table["name"], "fields_list": []}
+            if table_name not in tables_with_fields:
+                tables_with_fields[table_name] = []
 
-                        for column in table["columns"]:
-                            field_with_description = {"field_name" : column["name"], "field_description" : column["description"]}
-                            table_with_fields["fields_list"].append(field_with_description)
+            tables_with_fields[table_name].append((field_name, field_description, score))
 
-                            if column["references"] is not None:
-                                print(column["references"])
-                                referencies_list.append(f"'{table['name']}.{column['name']}' references {column['references']['table_name']}.{column['references']['field_name']}';\n")
+        prompt = "Similarity score with the user interrogation of each field in the database:\n\n"
 
-                        tables_with_fields_list.append(table_with_fields)
+        for table_name, fields in tables_with_fields.items():
+            prompt += f"TABLE '{table_name}':\n"
+            for field, description, score in fields:
+                prompt += f"- FIELD: {field} {' '*(max(len(field), 12) - len(field))} | SCORE: {score:.5f} | DESCRIPTION: {description}\n"
 
-            if len(tables_with_fields_list) > 0:
-                prompt += "The database contains the following tables:\n\n"
-
-                for table in tables_with_fields_list:
-                    prompt += f"table '{table['table_name']}' with fields:\n"
-
-                    for field in table['fields_list']:
-                        prompt += f"'{field['field_name']}' that contains {field['field_description']};\n"
-                    prompt += "\n"
-
-                if len(referencies_list) > 0:
-                    prompt += "and the database contains the following relationships:\n"
-                    for reference in referencies_list:
-                        prompt += reference
-
-                prompt += f"\nGenerate the SQL query equivalent to: {user_query}"
-                if prompt:
-                    pass
-                else:
-                    print("prompt not existent")
-                return prompt
+        return prompt
--- ChatSQL/Start/uploadServiceTemplate.py
+++ ChatSQL/Start/uploadServiceTemplate.py
@@ -15,9 +15,13 @@ class UploadServiceTemplace:
             index_name = os.path.splitext(dictionary_file_name)[0]
             index_path = os.path.join(self.emb.indexDirectory, index_name)
 
-            self.embe.index([{"table_name": command["table_name"], "table_description": command["table_description"],
-                "field_name": command["field_name"], "field_type": command["field_type"], "field_references": command["field_references"],
-                "text": command["table_description"]} for command in commands])                
+            self.embe.index([{"table_name": command["table_name"],
+                "table_description": command["table_description"],
+                "field_name": command["field_name"],
+                "field_type": command["field_type"],
+                "field_references": command["field_references"],
+                "field_description": command["field_description"],
+                "text": command["field_description"]} for command in commands])                
 
             self.embe.save(index_path)
             print("index path", index_path)
