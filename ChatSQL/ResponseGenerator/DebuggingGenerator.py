52a6493e1d75b503aca65183251189baa3464d9c
FedeYe <taorenfedericoye@gmail.com>
2024-03-29 11:08:01 +0100

    Salvataggfio modifiche nella response generation

--- ChatSQL/ResponseGenerator/DebuggingGenerator.py
+++ ChatSQL/ResponseGenerator/DebuggingGenerator.py
@@ -11,15 +11,44 @@ class DebuggingGenerator:
         pass
 
 
-#Output della funzione implementata
+# Output della funzione implementata
 """
 TABLE 'item':
 - FIELD 'id' | DESCRIPTION 'Unique identifier assigned to each auction item' | SCORE;
 - FIELD 'name' | DESCRIPTION 'Name of the auction item' | SCORE;
 - FIELD 'description' | DESCRIPTION 'Description of the auction item' | SCORE;
 - FIELD 'starting_bid' | DESCRIPTION 'Starting bid amount for the auction item' | SCORE;
+"""
+def generateResponse(emb, user_query):
 
+    embedder_search_result = emb.search(f"SELECT score, text, table_name, field_name, field_description FROM txtai WHERE similar('{user_query}') AND score > 0.01 GROUP BY table_name, field_name", limit=50)
 
+    # Gestione dizionari e loro percorsi ancora da implementare, dipende da che parametri passeremo alla funzione/cosa arriva dal controller
+    # Con l'implentazione attuale, per il debug basta avere l'emb con dentro caricato l'index corrente
+    # dictionary_path = os.path.join(getDictionariesFolderPath(), dictionary_name)
+
+    tables_with_fields = {}
+
+    for result in embedder_search_result:
+        table_name = result['table_name']
+        field_name = result['field_name']
+        field_description = result['field_description']
+        score = result['score']
+
+        if table_name not in tables_with_fields:
+            tables_with_fields[table_name] = []
+
+        tables_with_fields[table_name].append((field_name, field_description, score))
+
+    prompt = "Similarity score with the user interrogation of each field in the database:\n\n"
+
+    for table_name, fields in tables_with_fields.items():
+        prompt += f"TABLE '{table_name}':\n"
+        for field, description, score in fields:
+            prompt += f"- FIELD: {field} {' '*(max(len(field), 12) - len(field))} | SCORE: {score:.5f} | DESCRIPTION: {description}\n"
+
+    return prompt
+"""
 class Response(ABC):
 
     @abstractmethod
@@ -53,33 +82,3 @@ class ResponseModel:
     Response.generateResponse()
 
 """
-def generateResponse(emb, user_query, dictionary_path):
-
-
-    embedder_search_result = emb.search(f"SELECT score, text, table_name, field_name, field_description FROM txtai WHERE similar('{user_query}') AND score > 0.01 GROUP BY table_name, field_name", limit=50)
-
-    # Gestione dizionari e loro percorsi ancora da implementare, dipende da che paramtri passeremo alla funzione/cosa arriva dal controller
-    # Con l'implentazione attuale, per il debug basta avere l'emb con dentro caricato l'index corrente 
-    # dictionary_path = os.path.join(getDictionariesFolderPath(), dictionary_name)
-
-    tables_with_fields = {}
-
-    for result in embedder_search_result:
-        table_name = result['table_name']
-        field_name = result['field_name']
-        field_description = result['field_description']
-        score = result['score']
-
-        if table_name not in tables_with_fields:
-            tables_with_fields[table_name] = []
-
-        tables_with_fields[table_name].append((field_name, field_description, score ))
-
-    prompt = "Similarity score with the user interrogation of each field in the database:\n\n"
-
-    for table_name, fields in tables_with_fields.items():
-        prompt += f"TABLE '{table_name}':\n"
-        for field, description, score in fields:
-            prompt += f"- FIELD: {field} {' '*(max(len(field), 12) - len(field))} | SCORE: {score:.5f} | DESCRIPTION: {description}\n"
-
-    return prompt
--- ChatSQL/ResponseGenerator/ResponseGenerator.py
+++ ChatSQL/ResponseGenerator/ResponseGenerator.py
@@ -1,7 +1,32 @@
-from abc import ABC, abstractmethod
-
-class ResponseGeneratorInterface(ABC):
-
-    @abstractmethod
-    def generateResponse(emb, user_query, dictionary_path):
+class ResponseTechnician():
+    def __init__(self) -> None:
         pass
+
+    def generateResponse(emb, user_query):
+        embedder_search_result = emb.search(f"SELECT score, text, table_name, field_name, field_description FROM txtai WHERE similar('{user_query}') AND score > 0.01 GROUP BY table_name, field_name", limit=50)
+
+        # Gestione dizionari e loro percorsi ancora da implementare, dipende da che parametri passeremo alla funzione/cosa arriva dal controller
+        # Con l'implentazione attuale, per il debug basta avere l'emb con dentro caricato l'index corrente
+        # dictionary_path = os.path.join(getDictionariesFolderPath(), dictionary_name)
+
+        tables_with_fields = {}
+
+        for result in embedder_search_result:
+            table_name = result['table_name']
+            field_name = result['field_name']
+            field_description = result['field_description']
+            score = result['score']
+
+            if table_name not in tables_with_fields:
+                tables_with_fields[table_name] = []
+
+            tables_with_fields[table_name].append((field_name, field_description, score))
+
+        prompt = "Similarity score with the user interrogation of each field in the database:\n\n"
+
+        for table_name, fields in tables_with_fields.items():
+            prompt += f"TABLE '{table_name}':\n"
+            for field, description, score in fields:
+                prompt += f"- FIELD: {field} {' '*(max(len(field), 12) - len(field))} | SCORE: {score:.5f} | DESCRIPTION: {description}\n"
+
+        return prompt
