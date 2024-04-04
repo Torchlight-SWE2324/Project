6a3bc8d5911572b83073f08a5d4126dc38032dd6
Giovanni Filippini <39963191+giovannifil-64@users.noreply.github.com>
2024-04-04 11:58:24 +0000

    Fixed missing elements in the main

--- ChatSQL/main.py
+++ ChatSQL/main.py
@@ -3,27 +3,28 @@ from controller import *
 from widgets import *
 from embedder import Embedder
 
-# Usage
 if __name__ == "__main__":
 
     embedder = Embedder()
     dictionary_schema_verifier = JsonSchemaVerifierService()
+    responseUser = ResponseUser(embedder)
+    responseTechnician = ResponseTechnician(embedder)
 
     #modelli
     modelAut = ModelAuthentication()
     modelSel = ModelSelezione() 
     modelUp = ModelUpload(embedder, dictionary_schema_verifier)
     modelDel = ModelDelete()
-    modelCha = ModelChat()
+    modelCha = ModelChat(responseUser, responseTechnician)
 
     #controller
-    controllerAut = ControllerAuthentication(modelAut, None) #aut
-    controllerSel = ControllerSelezione(modelSel, None) #selezione 
+    controllerAut = ControllerAuthentication(modelAut, None)
+    controllerSel = ControllerSelezione(modelSel, None)
     controllerUp = ControllerUpload(modelUp, None)
     controllerDel = ControllerDelete(modelDel, None)
     controllerLog = ControllerLogout(modelAut, None)
     controllerCha = ControllerChat(modelCha, None)
-
+    
     #view
     loginWidget = LoginWidget(controllerAut)
     logoutWidget = LogoutWidget(controllerLog)    
@@ -31,7 +32,7 @@ if __name__ == "__main__":
     uploadWidget = UploadWidget(controllerUp)
     deleteWidget = DeleteWidget(selectWidget, controllerDel)
     chatWidget = ChatWidget(controllerCha, controllerSel, controllerAut)
-
+    
     #controller imposto view
     controllerAut._view = loginWidget
     controllerSel._view = selectWidget
@@ -43,15 +44,18 @@ if __name__ == "__main__":
     if "logged_in" not in st.session_state:
         st.session_state.logged_in = False
 
+    if "chat" not in st.session_state:
+        st.session_state.chat = []
+
     if st.session_state.logged_in == False:
         modelAut.setUtenteLoggato(False)
-        selectWidget.create()  #creazione widget selezione dizionario
-        chatWidget.create()    #deve essere messa come ultimo widget (non primo)
-        loginWidget.create()   #creazione widget login
-        
+        selectWidget.create()   #creazione widget selezione dizionario
+        chatWidget.create()     #deve essere messa come ultimo widget (non primo)
+        loginWidget.create()    #creazione widget login
+    
     else:
         modelAut.setUtenteLoggato(True)
-        deleteWidget.create()  #creazione widget delete (crea in automatico anche la selezione diz)
-        chatWidget.create()    #deve essere messa come ultimo widget (non primo)
-        uploadWidget.create()  #creazione widget selezione dizionario
-        logoutWidget.create()  #creazione widget selezione dizionario
+        deleteWidget.create()   #creazione widget delete (crea in automatico anche la selezione diz)
+        chatWidget.create()     #deve essere messa come ultimo widget (non primo)
+        uploadWidget.create()   #creazione widget selezione dizionario
+        logoutWidget.create()   #creazione widget selezione dizionario
